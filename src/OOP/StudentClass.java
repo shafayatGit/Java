bf611b47d13877f56e23400c8359a294179a5f12
shafayatGit <shafayathossain.ielts@gmail.com>
2025-08-07 20:13:46 +0600

    Classes In Different Files

--- src/OOP/StudentClass.java
+++ src/OOP/StudentClass.java
@@ -2,27 +2,43 @@ package OOP;
 
 import java.util.Scanner;
 
-
-
-
 public class StudentClass {
-//    Creating new data type
- public static class Student {
-    String name;
-    int rno;
-    double percent;
+//    public static void fun(Student x){ //scope error
+//        System.out.println(x.name); //
+//    }
+public static void fun(Stud x){ //That will not give scope error bcz of the outside calling
+   System.out.println(x.name); //
+   }
 
-}
+    public static class Stud {
+        String name;
+        int rno;
+        double percent;
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         Scanner sc = new Scanner(System.in); //Taking object using for taking input
+
+        class Student { //Declaring into main function. if I declare this into main function it will fall in scope
+            String name;
+            int rno;
+            double percent;
+
+        }
 
         Student x = new Student(); //declaration of object from class----> Class is Student and object is x
         x.name = "Shafayat";
         x.rno = 10;
         x.percent = 93.5;
         System.out.println("Name:"+x.name+" Roll:"+x.rno+" Percent:"+x.percent);
+        //fun(x); //i cant call this function because of the scope
+
+        Stud s1 = new Stud();
+        s1.name = "King";
+        fun(s1);
 
         Student y = new Student(); //Here class is Fixed which is Student, and we can make a lot of objects like x,y
         y.name = "Hossain";
